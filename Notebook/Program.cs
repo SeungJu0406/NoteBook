10cbf8e7eab4061b8ce8fd0013dc1c1c9b820e49
SeungJu0406 <nss8164@gmail.com>
2024-07-24 11:00:45 +0900

    제네릭 몬스터 제약조건 예시

--- Notebook/Program.cs
+++ Notebook/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Notebook
+﻿using System.Data.SqlTypes;
+
+namespace Notebook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Util.Monster orc1 = new Util.Monster("오크");
+            Util.Monster orc2 = new Util.Monster("오크");
+            if (Util.SameMonster(orc1, orc2)) 
+            {
+                Console.WriteLine("같은 몬스터입니다");
+            }
+            else
+            {
+                Console.WriteLine("다른 몬스터입니다");
+            }
         }
+
+
+    }
+    public static class Util
+    {
+        public class Monster
+        {
+            public string name;
+            public Monster(string name)
+            {
+                this.name = name;
+            }
+        }
+        public static bool SameMonster<T>(T left, T right) where T : Monster
+        {
+            return left.name == right.name;
+        }
+
     }
 }
