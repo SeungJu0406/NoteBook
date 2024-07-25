19d6499ff4432bffa983402110bd52c515a1d714
SeungJu0406 <nss8164@gmail.com>
2024-07-25 12:59:27 +0900

    델리게이트 체인, 이벤트

---
+++ Notebook/DelegateChain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleGate
+{
+    public class DelegateChain
+    {
+        public static void Main111()
+        {
+            Action action;
+            action = AAA;
+            action += BBB;
+            action += CCC;
+            action(); // 출력: ABC
+        }
+        public static void AAA()
+        {
+            Console.Write("A");
+        }
+        public static void BBB()
+        {
+            Console.Write("B");
+        }
+        public static void CCC()
+        {
+            Console.Write("C");
+        }
+    }
+}
--- Notebook/Event.cs
+++ Notebook/Event.cs
@@ -10,33 +10,45 @@ namespace DeleGate
     {
         public static void Main()
         {
-            Action action;
-            action = AAA;
-            action += BBB;
-            action += CCC;
-            action(); // 출력: ABC
+            Player player = new Player();
+            UI ui = new UI();
+            Sound sound = new Sound();
+            player.Ondied += ui.GameOverUI;
+            player.Ondied += sound.DeadSound;
+
+            player.Die();
         }
-        public static void AAA()
-        {
-            Console.Write("A");
-        }
-        public static void BBB()
-        {
-            Console.Write("B");
-        }
-        public static void CCC()
-        {
-            Console.Write("C");
-        }
+
         public class Player
         {
-            private void Die()
+            public event Action Ondied;
+            public void Die()
             {
                 // 나 주거따!!
+                Console.WriteLine("플레이어가 죽었습니다");
                 // >>
+                if(Ondied != null)
+                {
+                    Ondied(); // 일련이 사건이 발생했을떄 이벤트 발생
+                }
                 // 음악실행
                 // 게임오버 UI
                 // 등등 효과
+            }
+        }
+        
+        public class UI
+        {
+            public void GameOverUI()
+            {
+                Console.WriteLine("게임오버");
+            }
+        }
+        public class Sound
+        {
+            public void DeadSound()
+            {
+                Console.WriteLine("슬픈 bgm");
             }
         }
     }
