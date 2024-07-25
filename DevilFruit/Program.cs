85dae3a31c52236bdfdd626f2263cadfc674bc95
SeungJu0406 <nss8164@gmail.com>
2024-07-25 21:45:22 +0900

    공유용 코드

---
+++ DevilFruit/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DevilFruit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class DevilFruit
+        {
+            public void OceanCurse()
+            {
+                Console.WriteLine("바다의 저주를 받아 수영을 할 수 없습니다");
+            }
+        }
+
+        public class Rubber : DevilFruit
+        {
+
+        }
+    }
+}
