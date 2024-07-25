3937e0cf411b47c8567b24f53521859016b5b3c3
SeungJu0406 <nss8164@gmail.com>
2024-07-25 15:46:10 +0900

    확장메서드 예시코드

---
+++ method/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Additional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float value = 2.4f;
+            Console.WriteLine(value.Round());
+            string text = "hello world dasd";
+            Console.WriteLine(text.WordCount());
+        }
+        
+    }
+    public static class Extension
+    {
+        public static int Round(this float value) // 확장메서드
+        {
+            if (value % 1 >= 0.5f)
+            {
+                return (int)(value + 1);
+            }
+            else
+                return (int)value;
+        }
+        public static int WordCount(this string text)
+        {
+            string result=text.Replace(" ","");
+            return result.Length;
+        }
+    }
+}
