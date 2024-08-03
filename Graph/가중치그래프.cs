92d5b4ba0c3ebadc810bb504eca4dcc219a77135
SeungJu0406 <nss8164@gmail.com>
2024-08-03 18:37:24 +0900

    가중치 그래프 샘플 추가

---
+++ Graph/가중치그래프.cs
@@ -0,0 +1,116 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    static void Main()
+    {
+        ListGraph graph = new ListGraph(8);
+    }
+}
+
+public struct Vertex
+{
+    public string name;
+}
+
+public class ListGraph
+{
+    private List<int>[] graph;
+    private Vertex[] vertexs;
+
+    public int VertexCount => graph.Length;
+
+    public ListGraph(int vertexCount)
+    {
+        vertexs = new Vertex[vertexCount];
+        graph = new List<int>[vertexCount];
+        for (int i = 0; i < vertexCount; i++)
+        {
+            graph[i] = new List<int>();
+        }
+    }
+
+    public void Connect(int from, int to)
+    {
+        graph[from].Add(to);
+    }
+
+    public void DisConnect(int from, int to)
+    {
+        graph[from].Remove(to);
+    }
+
+    public bool IsConnect(int from, int to)
+    {
+        return graph[from].Contains(to);
+    }
+
+    public void SetVertex(int index, string name)
+    {
+        vertexs[index].name = name;
+    }
+
+    public Vertex GetVertex(int index)
+    {
+        return vertexs[index];
+    }
+}
+
+
+public class Graph
+{
+    struct Edge
+    {
+        public int dest;
+        public int weight;
+
+        public Edge(int dest, int weight)
+        {
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    private List<Edge>[] graph;
+
+    public Graph(int vertexCount)
+    {
+        graph = new List<Edge>[vertexCount];
+        for (int i = 0; i < vertexCount; i++)
+        {
+            graph[i] = new List<Edge>();
+        }
+    }
+
+    public void Connect(int from, int to, int weight)
+    {
+        Edge edge = new Edge(to, weight);
+        graph[from].Add(edge);
+    }
+
+    public void DisConnect(int from, int to)
+    {
+        for (int i = 0; i < graph[from].Count; i++)
+        {
+            if (graph[from][i].dest == to)
+            {
+                graph[from].RemoveAt(i);
+                return;
+            }
+        }
+    }
+
+    public int GetWeight(int from, int to)
+    {
+        for (int i = 0; i < graph[from].Count; i++)
+        {
+            if (graph[from][i].dest == to)
+            {
+                return graph[from][i].weight;
+            }
+        }
+
+        Console.WriteLine("간선이 없음");
+        return -1;
+    }
+}
--- Notebook/FileName.cs
+++ Notebook/FileName.cs
@@ -6,8 +6,45 @@ using System.Threading.Tasks;
 
 namespace DeleGate
 {
-    public abstract class DevilFruit
+    public class Program1
     {
-
+        static void Main(string[] args)
+        {
+            bool bigger = Compare(3, 2, Bigger); // compare에 Bigger 함수 넣어서 사용
+            bool less = Compare(3, 5, Less);
+            bool equal = Compare(3, 3, Equal);
+            int[] array1 = { 1, 3, 5, 7, 9 };
+            int less1 = CountIf(array1, 4, Less);
+            Console.WriteLine(less1);
+        }
+        public static bool Bigger(int left, int right)
+        {
+            return left > right;
+        }
+        public static bool Less(int left, int right)
+        {
+            return left < right;
+        }
+        public static bool Equal(int left, int right)
+        {
+            return left == right;
+        }
+        public static bool Compare(int left, int right, Func<int, int, bool> comperer)
+        // 델리게이트를 매개변수로 전달. comperer에 따라 결과 달라짐
+        {
+            return comperer(left, right);
+        }
+        public static int CountIf(int[] array, int value, Func<int, int, bool> comparer)
+        {
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item <= value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
