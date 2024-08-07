99a9d92fe4eb8d3ba10757e4fb4ed2fab0248880
SeungJu0406 <nss8164@gmail.com>
2024-08-07 13:11:41 +0900

    다익스트라 샘플

---
+++ Dijkstra/FileName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    public class Graph
+    {
+        public int[,] graph {  get; private set; }
+        public int INF {  get; private set; }
+        public Graph(int n)
+        {
+            INF = 999999;
+            graph = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    graph[i, j] = INF;
+                }
+            }
+        }
+        public void Connect(int from, int to, int value)
+        {
+            graph[from, to] = value;
+        }
+        public void ConnectBoth(int a , int b, int value)
+        {
+            graph[a, b] = value;
+            graph[b,a] = value;
+        }
+        public void DisConnect(int from, int to)
+        {
+            graph[from, to] = INF;
+        }
+        public bool IsConnect(int from , int to)
+        {
+            return graph[from, to] != INF ? true: false;
+        }
+    }
+}
---
+++ Dijkstra/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Dijkstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(8);
+            graph.ConnectBoth(0, 2, 1);
+            graph.ConnectBoth(0, 4, 2);
+            graph.ConnectBoth(2, 6, 5);
+            graph.ConnectBoth(4, 6, 4);
+            graph.ConnectBoth(6, 1, 7);
+            graph.ConnectBoth(6, 5, 8);
+            graph.ConnectBoth(6, 7, 4);
+            graph.ConnectBoth(1, 3, 5);
+            graph.ConnectBoth(1, 5, 2);
+            graph.ConnectBoth(3, 7, 6);
+
+            Dijkstra(graph, 0, out bool[] visited, out int[] distance, out int[] parent);
+            for (int i = 0; i < graph.graph.GetLength(0); i++)
+            {
+                Console.WriteLine($"{i}.{visited[i],8}{distance[i],8}{parent[i],8}");
+            }
+        }
+
+        public static void Dijkstra(/*그래프, 시작 정점, 방문?, 부모노드, */Graph graph, int start, out bool[] visited, out int[] distance, out int[] parent)
+        {
+            //방문, 부모노드 초기값
+            int size = graph.graph.GetLength(0);
+            visited = new bool[size];
+            parent = new int[size];
+            distance = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                visited[i] = false;
+                distance[i] = graph.INF;
+                parent[i] = -1;
+            }
+            distance[start] = 0;
+            for (int i = 0; i < size; i++)
+            {
+                int minIndex = -1;
+                int minCost = graph.INF;
+                //방문하지 않았으며, 가중치가 가장 낮은 노드 선정
+                for (int j = 0; j < size; j++)
+                {
+                    if (visited[j] == false &&
+                        distance[j] < minCost)
+                    {
+                        minIndex = j;
+                        minCost = distance[j];
+                    }
+                }
+                if (minIndex < 0) // 연결되어 있으면서 탐색한적이 없는 정점이 없었던 경우
+                    break;
+                visited[minIndex] = true;
+                //만약 합한 가중치의 합이 기존 가중치의 합보다 작은 경우 가중치의 값을 지정해주고, 부모노드를 교체
+                for (int j = 0; j < size; j++)
+                {
+                    if (distance[j] > distance[minIndex] + graph.graph[minIndex, j])
+                    {
+                        distance[j] = distance[minIndex] + graph.graph[minIndex, j];
+                        parent[j] = minIndex;
+                    }
+                }
+            }
+            //반복
+        }
+    }
+}
